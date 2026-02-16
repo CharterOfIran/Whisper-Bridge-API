86c2fb438bdc5044026b68af08c76cc5756194d3
HamSarNeveshti <253335627+CharterOfIran@users.noreply.github.com>
2026-02-16 19:33:40 +0000

    Update sanitizer.go

--- internal/security/sanitizer.go
+++ internal/security/sanitizer.go
@@ -1,15 +1,17 @@
+package security
+
 import "regexp"
 
 func Clean(msg string) string {
-    // ۱. اول محدودیت طول (جلوگیری از پر شدن RAM)
-    if len(msg) > 5000 {
-        msg = msg[:5000]
-    }
+	// ۱. اول محدودیت طول (جلوگیری از پر شدن RAM)
+	if len(msg) > 5000 {
+		msg = msg[:5000]
+	}
 
-    // ۲. فقط اجازه دادن به کاراکترهای مجاز (Regex White-list)
-    // این ریجکس حروف فارسی، انگلیسی، اعداد و علائم معمولی رو نگه میداره
-    reg := regexp.MustCompile(`[^a-zA-Z0-9آ-ی\s.,!؟?()]+`)
-    clean := reg.ReplaceAllString(msg, "")
+	// ۲. فقط اجازه دادن به کاراکترهای مجاز (Regex White-list)
+	// این ریجکس حروف فارسی، انگلیسی، اعداد و علائم معمولی رو نگه میداره
+	reg := regexp.MustCompile(`[^a-zA-Z0-9آ-ی\s.,!؟?()]+`)
+	clean := reg.ReplaceAllString(msg, "")
 
-    return clean
+	return clean
 }
